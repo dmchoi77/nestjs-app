ead8b2b49b8742ba16c5ca469a37dfb6976bc2d1
dm.choi <dm.choi@logiall.com>
2023-12-22 01:27:46 +0900

    users, email service 작성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,18 +3,24 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserInfo } from './UserInfo';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  // UsersService를 컨트롤러에 주입
+  constructor(private usersService: UsersService) {}
+
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
-    console.log(dto);
+    const { email, name, password } = dto;
+    await this.usersService.createUser(name, email, password);
   }
 
   @Post('/email-verify')
   async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
-    console.log(dto);
-    return;
+    const { signupVerifyToken } = dto;
+
+    return await this.usersService.verifyEmail(signupVerifyToken);
   }
 
   @Post('/login')
---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import * as uuid from 'uuid';
+import { Injectable } from '@nestjs/common';
+import { EmailService } from 'src/email/email.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private emailService: EmailService) {}
+
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExists(email);
+
+    const signupVerifyToken = uuid.v1();
+
+    await this.saveUser(name, email, password, signupVerifyToken);
+    await this.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+
+  private checkUserExists(email: string) {
+    return false; // TODO: DB 연동 후 구현
+  }
+
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    return; // TODO: DB 연동 후 구현
+  }
+
+  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+    await this.emailService.sendMemberJoinVerification(
+      email,
+      signupVerifyToken,
+    );
+  }
+
+  async verifyEmail(signupVerifyToken: string): Promise<string> {
+    // TODO:
+    // 1. DB에서 signupVerifyToken으로 회원 가입 처리 중인 유저가 있는지 조회하고 없다면 에러 처리
+    // 2. 바로 로그인 상태가 되도록 JWT 발급
+    throw new Error('');
+  }
+}
