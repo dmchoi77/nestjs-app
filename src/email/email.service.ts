debd87aaf2aa496a5bbff8126dd5a906e754f5a2
dm.choi <dm.choi@logiall.com>
2023-12-23 20:21:07 +0900

    emailConfig 주입

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,7 +1,9 @@
+import { ConfigType } from '@nestjs/config';
 import Mail = require('nodemailer/lib/mailer');
 import * as nodemailer from 'nodemailer';
 
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import emailConfig from 'src/config/emailConfig';
 
 interface EmailOptions {
   to: string;
@@ -12,12 +14,14 @@ interface EmailOptions {
 @Injectable()
 export class EmailService {
   private transporter: Mail;
-  constructor() {
+  constructor(
+    @Inject(emailConfig.KEY) private config: ConfigType<typeof emailConfig>,
+  ) {
     this.transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: config.service,
       auth: {
-        user: '',
-        pass: '',
+        user: config.auth.user,
+        pass: config.auth.pass,
       },
     });
   }
@@ -25,7 +29,7 @@ export class EmailService {
     emailAddress: string,
     signupVerifyToken: string,
   ) {
-    const baseUrl = 'http://localhost:3100';
+    const baseUrl = this.config.baseUrl;
 
     const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
 
