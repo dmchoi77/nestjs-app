716c94bed66f451caed9cbcc3d3c3267f6f40cad
dm.choi <dm.choi@logiall.com>
2023-12-25 17:23:41 +0900

    CreateUserDto validation 추가, NotIn 커스텀 validation 작성

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,33 @@
+import {
+  IsString,
+  MinLength,
+  MaxLength,
+  IsEmail,
+  Matches,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+import { BadRequestException } from '@nestjs/common';
 export class CreateUserDto {
+  @Transform((params) => params.value.trim())
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string;
+
+  @Transform(({ value, obj }) => {
+    if (obj.password.includes(obj.name.trim())) {
+      throw new BadRequestException(
+        '패스워드는 name과 같은 문자열을 포함할 수 없습니다.',
+      );
+    }
+    return value.trim();
+  })
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   readonly email: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   readonly password: string;
 }
