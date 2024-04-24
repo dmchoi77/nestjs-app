d1b28895bf1ec9c091f57fa1d17ce4e6e86657a7
최동민 <dm.choi@logiall.com>
2024-04-24 23:39:45 +0900

    메모 삭제 서비스 작성 외 기타

--- src/memos/memos.service.ts
+++ src/memos/memos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateMemoDto } from './dto/create-memo.dto';
@@ -8,32 +8,41 @@ import { MemoEntity } from './entities/memo.entity';
 export class MemosService {
   constructor(
     @InjectRepository(MemoEntity)
-    private memoRepository: Repository<MemoEntity>,
+    private memosRepository: Repository<MemoEntity>,
   ) {}
 
   async createMemo(user: string, createMemoDto: CreateMemoDto) {
     const { content, date } = createMemoDto;
 
-    const lastMemo = await this.memoRepository.findOne({
+    const lastMemo = await this.memosRepository.findOne({
       where: { date, user: user },
       order: { displayOrder: 'DESC' },
     });
 
-    const newMemo = this.memoRepository.create({
+    const newMemo = this.memosRepository.create({
       content,
       date,
       user: user,
       displayOrder: lastMemo ? lastMemo.displayOrder + 1 : 0,
     });
 
-    return this.memoRepository.save(newMemo);
+    return this.memosRepository.save(newMemo);
   }
 
-  async fetchMemoList(user: string, date: Date) {
-    return await this.memoRepository
+  fetchMemoList(user: string, date: Date) {
+    return this.memosRepository
       .createQueryBuilder('memo')
       .select(['memo.content', 'memo.id'])
       .where({ user: user, date: date })
       .getMany();
+  }
+
+  async deleteMemo(memoId: number) {
+    const memo = await this.memosRepository.findOne({
+      where: { id: memoId },
+    });
+    if (!memo) throw new NotFoundException('메모가 존재하지 않습니다.');
+
+    await this.memosRepository.delete({ id: memoId });
   }
 }
